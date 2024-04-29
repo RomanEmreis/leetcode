738ad02a099a1fa12d261935cbbd74c50930287b
Roman <39233213+RomanEmreis@users.noreply.github.com>
2024-04-29 13:38:21 +0400

    Create min_ops_num_to_make_xor_k.cpp

---
+++ bitwise/min_ops_num_to_make_xor_k.cpp
@@ -0,0 +1,36 @@
+/*
+  You are given a 0-indexed integer array nums and a positive integer k.
+
+  You can apply the following operation on the array any number of times:
+
+  Choose any element of the array and flip a bit in its binary representation. Flipping a bit means changing a 0 to 1 or vice versa.
+  Return the minimum number of operations required to make the bitwise XOR of all elements of the final array equal to k.
+
+  Note that you can flip leading zero bits in the binary representation of elements. For example, for the number (101)2 you can flip the fourth bit and obtain (1101)2.
+*/
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int result = 0;
+
+        for (int i = 0; i < nums.size(); ++i) result ^= nums[i];
+        if (result == k) return 0;
+
+        int count = 0;
+        int diff = result ^ k;
+
+        while (diff > 0) {
+            if (diff & 1) ++count;
+            diff >>= 1;
+        }
+
+        return count;
+    }
+};
+
+static auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
