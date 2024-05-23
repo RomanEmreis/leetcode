03d16fe6c5d107064e773f12a4e1a864a61d5c41
Roman <39233213+RomanEmreis@users.noreply.github.com>
2024-05-23 14:24:43 +0400

    Create num_of_beautiful_subsets.cs

---
+++ dfs/num_of_beautiful_subsets.cs
@@ -0,0 +1,41 @@
+/*
+  You are given an array nums of positive integers and a positive integer k.
+  A subset of nums is beautiful if it does not contain two integers with an absolute difference equal to k.
+
+  Return the number of non-empty beautiful subsets of the array nums.
+
+  A subset of nums is an array that can be obtained by deleting some (possibly none) elements from nums. Two subsets are different if and only if the chosen indices to delete are different.
+
+  Example:
+    Input: nums = [2,4,6], k = 2
+    Output: 4
+    Explanation: The beautiful subsets of the array nums are: [2], [4], [6], [2, 6].
+    It can be proved that there are only 4 beautiful subsets in the array [2,4,6].
+*/
+public class Solution {
+    public int BeautifulSubsets(int[] nums, int k) {
+        if(nums.Length <= 1) return nums.Length;
+
+        int result = 0;
+        List<int> subset = [];
+        Dfs(0);
+
+        return result - 1;
+
+        void Dfs(int index) {
+            if (index >= nums.Length) {
+                ++result;
+                return;
+            }
+
+            if (!subset.Contains(nums[index] + k) && !subset.Contains(nums[index] - k)) {
+                subset.Add(nums[index]);
+                Dfs(index + 1);
+                subset.RemoveAt(subset.Count - 1);
+                Dfs(index + 1);
+            } else {
+                Dfs(index + 1);
+            }
+        }
+    }
+}
