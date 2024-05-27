833fcbd656c8026ef3e53923b6fed1bc2e27cd41
Roman <39233213+RomanEmreis@users.noreply.github.com>
2024-05-27 19:23:51 +0400

    Create special_array_with_x_elements_greater_or_equal_x.cs

---
+++ binary_search/special_array_with_x_elements_greater_or_equal_x.cs
@@ -0,0 +1,30 @@
+/*
+  You are given an array nums of non-negative integers. nums is considered special if there exists a number x such that there are exactly x numbers in nums that are greater than or equal to x.
+  Notice that x does not have to be an element in nums.
+
+  Return x if the array is special, otherwise, return -1. It can be proven that if nums is special, the value for x is unique.
+
+  Example:
+    Input: nums = [3,5]
+    Output: 2
+    Explanation: There are 2 values (3 and 5) that are greater than or equal to 2.
+*/
+public class Solution {
+    public int SpecialArray(int[] nums) {
+        Array.Sort(nums, new Comparison<int>((x, y) => y.CompareTo(x)));
+
+        int n = nums.Length;
+        if (nums[n - 1] > n) return n;
+        if (nums[0] == 0) return -1;
+
+        int l = 0, r = n - 1, m = 0;
+        while (l <= r) {
+            m = l + (r - l) / 2;
+            if (nums[m] > m) l = m + 1;
+            else if (nums[m] < m) r = m - 1;
+            else return -1; 
+        }
+
+        return nums[m] > m ? m + 1 : m;
+    }
+}
