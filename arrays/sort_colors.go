9738ea1f0fb765be50707020b73c2df6f538aa3b
Roman <39233213+RomanEmreis@users.noreply.github.com>
2024-06-12 17:55:26 +0400

    Create sort_colors.go

---
+++ arrays/sort_colors.go
@@ -0,0 +1,19 @@
+/*
+  Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+  We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+  You must solve this problem without using the library's sort function.
+*/
+func sortColors(nums []int)  {
+    count := make([]int, 3);
+    for _, n := range nums {
+        count[n]++;
+    }
+
+    for i, idx := 0, 0; i < 3; i++ {
+        for j := 0; j < count[i]; j++ {
+            nums[idx] = i;
+            idx++;
+        }
+    }
+}
