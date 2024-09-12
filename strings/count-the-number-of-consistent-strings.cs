5ade46b72a7d680a4f2ed07fac8fd868f2fb2962
Roman <39233213+RomanEmreis@users.noreply.github.com>
2024-09-12 11:58:39 +0400

    Create count-the-number-of-consistent-strings.cs

---
+++ strings/count-the-number-of-consistent-strings.cs
@@ -0,0 +1,18 @@
+/*
+  You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+  
+  Return the number of consistent strings in the array words.
+  
+  Example 1:
+    Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+    Output: 2
+    Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+*/
+public class Solution {
+    public int CountConsistentStrings(string allowed, string[] words) {
+        HashSet<char> set = allowed.ToHashSet();
+        return words.Count(word => 
+            word.All(ch => set.Contains(ch))
+        );
+    }
+}
