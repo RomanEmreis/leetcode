d420e0815e9455475a8e1659080d561052c1f7d2
Roman <39233213+RomanEmreis@users.noreply.github.com>
2024-09-26 17:32:29 +0400

    Create my-calendar-i.cs

---
+++ trees/my-calendar-i.cs
@@ -0,0 +1,69 @@
+/*
+  You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
+  A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+  The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+  
+  Implement the MyCalendar class:
+  MyCalendar() Initializes the calendar object.
+  boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+   
+  
+  Example 1:
+    Input
+    ["MyCalendar", "book", "book", "book"]
+    [[], [10, 20], [15, 25], [20, 30]]
+    Output
+    [null, true, false, true]
+    
+    Explanation
+    MyCalendar myCalendar = new MyCalendar();
+    myCalendar.book(10, 20); // return True
+    myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+    myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+*/
+public sealed class MyCalendar {
+    private Node? root;
+
+    public MyCalendar() {
+        root = null;
+    }
+    
+    public bool Book(int start, int end) {
+        if (root is null) {
+            root = new(start, end);
+            return true;
+        }
+        return Insert(root, start, end);
+    }
+
+    public bool Insert(Node node, int start, int end) {
+        if (end <= node.Start) {
+            if (node.Left is null) {
+                node.Left = new(start, end);
+                return true;
+            }
+            return Insert(node.Left, start, end);
+        } else if (start >= node.End) {
+            if (node.Right is null) {
+                node.Right = new(start, end);
+                return true;
+            }
+            return Insert(node.Right, start, end);
+        }
+        return false;
+    }
+}
+
+public sealed class Node(int start, int end) {
+    public int Start = start;
+    public int End = end;
+
+    public Node Left;
+    public Node Right;
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * bool param_1 = obj.Book(start,end);
+ */
