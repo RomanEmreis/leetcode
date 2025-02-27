986be56739b06cbd43c48cd14fc6d4cefd612902
Roman <39233213+RomanEmreis@users.noreply.github.com>
2025-02-27 12:58:52 +0400

    Create length-of-longest-fibonacci-subsequence.cs

---
+++ hash_map/length-of-longest-fibonacci-subsequence.cs
@@ -0,0 +1,45 @@
+/*
+  873. Length of Longest Fibonacci Subsequence
+  
+  A sequence x1, x2, ..., xn is Fibonacci-like if:
+  n >= 3
+  xi + xi+1 == xi+2 for all i + 2 <= n
+  Given a strictly increasing array arr of positive integers forming a sequence, return the length of the longest Fibonacci-like subsequence of arr. If one does not exist, return 0.
+  
+  A subsequence is derived from another sequence arr by deleting any number of elements (including none) from arr, without changing the order of the remaining elements. For example, [3, 5, 8] is a subsequence of [3, 4, 5, 6, 7, 8].
+  
+  Example 1:
+  Input: arr = [1,2,3,4,5,6,7,8]
+  Output: 5
+  Explanation: The longest subsequence that is fibonacci-like: [1,2,3,5,8].
+  
+  Example 2:
+  Input: arr = [1,3,7,11,12,14,18]
+  Output: 3
+  Explanation: The longest subsequence that is fibonacci-like: [1,11,12], [3,11,14] or [7,11,18].
+*/
+public class Solution {
+    public int LenLongestFibSubseq(int[] arr) {
+        int n = arr.Length;
+        Dictionary<int, int> map1 = [];
+        Dictionary<(int, int), int> map2 = [];
+
+        for (int i = 0; i < arr.Length; ++i) {
+            map1[arr[i]] = i;
+        }
+
+        int result = 0;
+        for (int i = 2; i < arr.Length; ++i) {
+            for (int j = 1; j < i; ++j) {
+                int d = arr[i] - arr[j];
+                if (d < arr[j] && map1.TryGetValue(d, out int x)) {
+                    int l = map2.GetValueOrDefault((x, j), 2) + 1;
+                    map2[(j, i)] = l;
+                    result = Math.Max(result, l);
+                }
+            }
+        }
+        return result >= 3 ? result : 0;
+    }
+}
+
