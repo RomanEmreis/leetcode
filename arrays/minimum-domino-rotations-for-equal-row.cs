8cdd909d95c91cb90c5093f880dde97fe958d18e
Roman <39233213+RomanEmreis@users.noreply.github.com>
2025-05-03 13:04:22 +0400

    Create minimum-domino-rotations-for-equal-row.cs

---
+++ arrays/minimum-domino-rotations-for-equal-row.cs
@@ -0,0 +1,41 @@
+/*
+  1007. Minimum Domino Rotations For Equal Row
+  
+  In a row of dominoes, tops[i] and bottoms[i] represent the top and bottom halves of the ith domino. (A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
+  We may rotate the ith domino, so that tops[i] and bottoms[i] swap values.
+  
+  Return the minimum number of rotations so that all the values in tops are the same, or all the values in bottoms are the same.
+  If it cannot be done, return -1.
+  
+  Example 1:
+  Input: tops = [2,1,2,4,2,2], bottoms = [5,2,6,2,3,2]
+  Output: 2
+  Explanation: 
+  The first figure represents the dominoes as given by tops and bottoms: before we do any rotations.
+  If we rotate the second and fourth dominoes, we can make every value in the top row equal to 2, as indicated by the second figure.
+  
+  Example 2:
+  Input: tops = [3,5,1,2,3], bottoms = [3,6,3,3,4]
+  Output: -1
+  Explanation: 
+  In this case, it is not possible to rotate the dominoes to make one row of values equal.
+*/
+public class Solution {
+    public int MinDominoRotations(int[] tops, int[] bottoms) {
+        int n = tops.Length;
+        int result = Math.Min(Calc(tops[0]), Calc(bottoms[0]));
+
+        return result > n ? -1 : result;
+
+        int Calc(int x) {
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < n; ++i) {
+                if (tops[i] != x && bottoms[i] != x) return n + 1;
+                if (tops[i] == x) ++a;
+                if (bottoms[i] == x) ++b;
+            }
+            return n - Math.Max(a, b);
+        }
+    }
+}
