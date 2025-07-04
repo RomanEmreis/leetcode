315e5596dccb39d9fa640a35d2164b483d4ecf83
Roman <39233213+RomanEmreis@users.noreply.github.com>
2025-07-04 13:01:28 +0400

    Create find-the-k-th-character-in-string-game-ii.rs

---
+++ binary/find-the-k-th-character-in-string-game-ii.rs
@@ -0,0 +1,46 @@
+/*
+  3307. Find the K-th Character in String Game II
+  
+  Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+  You are given a positive integer k. You are also given an integer array operations, where operations[i] represents the type of the ith operation.
+  
+  Now Bob will ask Alice to perform all operations in sequence:
+  If operations[i] == 0, append a copy of word to itself.
+  If operations[i] == 1, generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original word. For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+  Return the value of the kth character in word after performing all the operations.
+  
+  Note that the character 'z' can be changed to 'a' in the second type of operation.
+  
+  Example 1:
+  Input: k = 5, operations = [0,0,0]
+  Output: "a"
+  Explanation:
+  Initially, word == "a". Alice performs the three operations as follows:
+  Appends "a" to "a", word becomes "aa".
+  Appends "aa" to "aa", word becomes "aaaa".
+  Appends "aaaa" to "aaaa", word becomes "aaaaaaaa".
+  
+  Example 2:
+  Input: k = 10, operations = [0,1,0,1]
+  Output: "b"
+  Explanation:
+  Initially, word == "a". Alice performs the four operations as follows:
+  Appends "a" to "a", word becomes "aa".
+  Appends "bb" to "aa", word becomes "aabb".
+  Appends "aabb" to "aabb", word becomes "aabbaabb".
+  Appends "bbccbbcc" to "aabbaabb", word becomes "aabbaabbbbccbbcc".
+*/
+impl Solution {
+    pub fn kth_character(mut k: i64, operations: Vec<i32>) -> char {
+        let mut result = 0;
+        while k != 1 {
+            let t = 63 - k.leading_zeros();
+            let t = if (1 << t) == k { t - 1 } else { t };
+            k -= 1 << t;
+            if operations[t as usize] != 0 {
+                result += 1;
+            }
+        }
+        ((result % 26) as u8 + b'a') as char
+    }
+}
