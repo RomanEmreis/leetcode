b2e7afc62dfe1ad638b79fa17209c68a0283d9a5
Roman <39233213+RomanEmreis@users.noreply.github.com>
2025-10-14 11:22:15 +0400

    Create adjacent-increasing-subarrays-detection-i.rs

---
+++ arrays/adjacent-increasing-subarrays-detection-i.rs
@@ -0,0 +1,48 @@
+/*
+  3349. Adjacent Increasing Subarrays Detection I
+  
+  Given an array nums of n integers and an integer k, determine whether there exist two adjacent
+  of length k such that both subarrays are strictly increasing. Specifically, check if there are two subarrays starting at indices a and b (a < b), where:
+      Both subarrays nums[a..a + k - 1] and nums[b..b + k - 1] are strictly increasing.
+      The subarrays must be adjacent, meaning b = a + k.
+  
+  Return true if it is possible to find two such subarrays, and false otherwise.
+  
+  Example 1:
+  Input: nums = [2,5,7,8,9,2,3,4,3,1], k = 3
+  Output: true
+  Explanation:
+      The subarray starting at index 2 is [7, 8, 9], which is strictly increasing.
+      The subarray starting at index 5 is [2, 3, 4], which is also strictly increasing.
+      These two subarrays are adjacent, so the result is true.
+  
+  Example 2:
+  Input: nums = [1,2,3,4,4,4,4,5,6,7], k = 5
+  Output: false
+*/
+impl Solution {
+    pub fn has_increasing_subarrays(nums: Vec<i32>, k: i32) -> bool {
+        let n = nums.len();
+        let k = k as usize;
+        let mut l = 0;
+        let mut r = k * 2;
+        while r <= n {
+            if is_increasing(&nums[l..l + k]) && is_increasing(&nums[l + k..r]) {
+                return true;
+            }
+            l += 1;
+            r += 1;
+        }
+        false
+    }
+}
+
+#[inline]
+fn is_increasing(arr: &[i32]) -> bool {
+    for i in 1..arr.len() {
+        if arr[i - 1] >= arr[i] {
+            return false;
+        }
+    }
+    true
+}
