971381d58dd6ee6bca5a4c993712b7f1de52de4c
Roman <39233213+RomanEmreis@users.noreply.github.com>
2025-10-24 11:18:10 +0400

    Create next-greater-numerically-balanced-number.rs

---
+++ backtracking/next-greater-numerically-balanced-number.rs
@@ -0,0 +1,62 @@
+/*
+  2048. Next Greater Numerically Balanced Number
+  
+  An integer x is numerically balanced if for every digit d in the number x, there are exactly d occurrences of that digit in x.
+  Given an integer n, return the smallest numerically balanced number strictly greater than n.
+  
+  Example 1:
+  Input: n = 1
+  Output: 22
+  Explanation: 
+  22 is numerically balanced since:
+  - The digit 2 occurs 2 times. 
+  It is also the smallest numerically balanced number strictly greater than 1.
+  
+  Example 2:
+  Input: n = 1000
+  Output: 1333
+  Explanation: 
+  1333 is numerically balanced since:
+  - The digit 1 occurs 1 time.
+  - The digit 3 occurs 3 times. 
+  It is also the smallest numerically balanced number strictly greater than 1000.
+  Note that 1022 cannot be the answer because 0 appeared more than 0 times.
+  
+  Example 3:
+  Input: n = 3000
+  Output: 3133
+  Explanation: 
+  3133 is numerically balanced since:
+  - The digit 1 occurs 1 time.
+  - The digit 3 occurs 3 times.
+  It is also the smallest numerically balanced number strictly greater than 3000.
+*/
+impl Solution {
+    pub fn next_beautiful_number(mut n: i32) -> i32 {
+        fn is_balanced(mut n: i32) -> bool {
+            let mut cnt = [0; 10];
+            while n > 0 {
+                let d = n % 10;
+                if d == 0 {
+                    return false;
+                }
+                cnt[d as usize] += 1;
+                n /= 10;
+            }
+            for i in 1..10 {
+                if cnt[i] != 0 && cnt[i] != i {
+                    return false;
+                }
+            }
+            true
+        }
+        
+        loop {
+            n += 1;
+            if is_balanced(n) {
+                break;
+            }
+        }
+        n
+    }
+}
