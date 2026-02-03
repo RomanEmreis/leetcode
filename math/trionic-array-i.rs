fee6b31f73cb859b6ded8aa200a5f11327b7ff44
Roman <39233213+RomanEmreis@users.noreply.github.com>
2026-02-03 10:55:38 +0400

    Add trionic array check implementation
    Implement function to check if an array is trionic based on specified conditions.

---
+++ math/trionic-array-i.rs
@@ -0,0 +1,56 @@
+/*
+  3637. Trionic Array I
+  
+  You are given an integer array nums of length n.
+  An array is trionic if there exist indices 0 < p < q < n − 1 such that:
+      nums[0...p] is strictly increasing,
+      nums[p...q] is strictly decreasing,
+      nums[q...n − 1] is strictly increasing.
+  
+  Return true if nums is trionic, otherwise return false.
+  
+  Example 1:
+  Input: nums = [1,3,5,4,2,6]
+  Output: true
+  Explanation:
+  Pick p = 2, q = 4:
+      nums[0...2] = [1, 3, 5] is strictly increasing (1 < 3 < 5).
+      nums[2...4] = [5, 4, 2] is strictly decreasing (5 > 4 > 2).
+      nums[4...5] = [2, 6] is strictly increasing (2 < 6).
+  
+  Example 2:
+  Input: nums = [2,1,3]
+  Output: false
+  Explanation:
+  There is no way to pick p and q to form the required three segments.
+*/
+impl Solution {
+    pub fn is_trionic(nums: Vec<i32>) -> bool {
+        let n = nums.len();
+        
+        let mut p = 0;
+        while p < n - 2 && nums[p] < nums[p + 1] {
+            p += 1;
+        }
+
+        if p == 0 {
+            return false;
+        }
+
+        let mut q = p;
+        while q < n - 1 && nums[q] > nums[q + 1] {
+            q += 1;
+        }
+
+        if q == p || q == n - 1 {
+            return false;
+        }
+
+        let mut i = q;
+        while i < n - 1 && nums[i] < nums[i + 1] {
+            i += 1;
+        }
+
+        i == n - 1
+    }
+}
