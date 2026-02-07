f2e928ae53e5908ca15449e2d904a66e9b645c96
Roman <39233213+RomanEmreis@users.noreply.github.com>
2026-02-07 11:35:15 +0400

    Add solution for minimum deletions to balance string
    Implement solution to calculate minimum deletions needed to balance a string of 'a's and 'b's.

---
+++ strings/minimum-deletions-to-make-string-balanced.rs
@@ -0,0 +1,37 @@
+/*
+  1653. Minimum Deletions to Make String Balanced
+  
+  You are given a string s consisting only of characters 'a' and 'b'​​​​.
+  You can delete any number of characters in s to make s balanced. s is balanced 
+  if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+  
+  Return the minimum number of deletions needed to make s balanced.
+  
+  Example 1:
+  Input: s = "aababbab"
+  Output: 2
+  Explanation: You can either:
+  Delete the characters at 0-indexed positions 2 and 6 ("aababbab" -> "aaabbb"), or
+  Delete the characters at 0-indexed positions 3 and 6 ("aababbab" -> "aabbbb").
+  
+  Example 2:
+  Input: s = "bbaaaaabb"
+  Output: 2
+  Explanation: The only solution is to delete the first two characters.
+*/
+impl Solution {
+    pub fn minimum_deletions(s: String) -> i32 {
+        let mut b = 0;
+        let mut res = 0;
+
+        for c in s.into_bytes() {
+            if c == b'b' {
+                b += 1;
+            } else {
+                res = b.min(res + 1);
+            }
+        }
+        
+        res
+    }
+}
