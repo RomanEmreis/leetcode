1de74453c4c22a9999c19c45d684e02e1a219555
Roman <39233213+RomanEmreis@users.noreply.github.com>
2026-03-21 11:59:50 +0400

    Add function to flip square submatrix vertically
    Implement a function to reverse a square submatrix vertically in a given grid.

---
+++ arrays/flip-square-submatrix-vertically.rs
@@ -0,0 +1,36 @@
+/*
+  3643. Flip Square Submatrix Vertically
+  
+  You are given an m x n integer matrix grid, and three integers x, y, and k.
+  The integers x and y represent the row and column indices of the top-left corner of a square submatrix 
+  and the integer k represents the size (side length) of the square submatrix.
+  Your task is to flip the submatrix by reversing the order of its rows vertically.
+  
+  Return the updated matrix.
+  
+  Example 1:
+  Input: grid = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], x = 1, y = 0, k = 3
+  Output: [[1,2,3,4],[13,14,15,8],[9,10,11,12],[5,6,7,16]]
+  Explanation:
+  The diagram above shows the grid before and after the transformation.
+  
+  Example 2:
+  Input: grid = [[3,4,2,3],[2,3,4,2]], x = 0, y = 2, k = 2
+  Output: [[3,4,4,2],[2,3,2,3]]
+  Explanation:
+  The diagram above shows the grid before and after the transformation.
+*/
+impl Solution {
+    pub fn reverse_submatrix(mut grid: Vec<Vec<i32>>, x: i32, y: i32, k: i32) -> Vec<Vec<i32>> {
+        let k = k as usize;
+        let x = x as usize;
+        let y = y as usize;
+
+        for i in 0..k / 2 {
+            for j in 0..k {
+                (grid[x + i][y + j], grid[x + k - i - 1][y + j]) = (grid[x + k - i - 1][y + j], grid[x + i][y + j]);
+            }
+        }
+        grid
+    }
+}
