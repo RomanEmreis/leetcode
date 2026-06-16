2c6174b99e7e2c34ce4e4f42dcf514106a7e4493
Roman <39233213+RomanEmreis@users.noreply.github.com>
2026-06-16 12:43:32 +0400

    Create process-string-with-special-operations-i.rs

---
+++ strings/process-string-with-special-operations-i.rs
@@ -0,0 +1,50 @@
+/*
+  3612. Process String with Special Operations I
+  
+  You are given a string s consisting of lowercase English letters and the special characters: *, #, and %.
+  Build a new string result by processing s according to the following rules from left to right:
+      If the letter is a lowercase English letter append it to result.
+      A '*' removes the last character from result, if it exists.
+      A '#' duplicates the current result and appends it to itself.
+      A '%' reverses the current result.
+  
+  Return the final string result after processing all characters in s.
+  
+  Example 1:
+  Input: s = "a#b%*"
+  Output: "ba"
+  Explanation:
+  i	s[i]	Operation	Current result
+  0	'a'	Append 'a'	"a"
+  1	'#'	Duplicate result	"aa"
+  2	'b'	Append 'b'	"aab"
+  3	'%'	Reverse result	"baa"
+  4	'*'	Remove the last character	"ba"
+  
+  Thus, the final result is "ba".
+  
+  Example 2:
+  Input: s = "z*#"
+  Output: ""
+  Explanation:
+  i	s[i]	Operation	Current result
+  0	'z'	Append 'z'	"z"
+  1	'*'	Remove the last character	""
+  2	'#'	Duplicate the string	""
+  
+  Thus, the final result is "".
+*/
+impl Solution {
+    pub fn process_str(s: String) -> String {
+        let mut res = String::with_capacity(s.len());
+        for ch in s.chars() {
+            match ch {
+                '*' => _ = res.pop(),
+                '#' => res.extend_from_within(0..),
+                '%' => res = res.chars().rev().collect(),
+                _ => res.push(ch)
+            }
+        }
+        res
+    }
+}
